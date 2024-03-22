a7f4a9dfe7b38637de2da5fdd03f2eb9a911dba1
KhalidAhmad <Khalid31894@gmail.com>
2024-03-23 02:58:08 +0500

    del files

--- Assets/feat_SpinWheel/SO/Octant_SO/Octant_List_SO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class Octant_List_SO : ScriptableObject
-{
-    public List<Octant_SO> octant_SOs = new List<Octant_SO>();
-}
